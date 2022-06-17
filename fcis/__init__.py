cc5437ed5b30b6e8d418f6fe3ae9c1bdf1796901
jwc20 <choij2015@gmail.com>
2022-06-17 15:11:57 -0700

    initialize

---
+++ fcis/__init__.py
@@ -0,0 +1,20 @@
+from .core import *
+from .results import Results
+from .accident import Accident
+from .inspection import Inspection
+from .sic import Sic # Standard Industrial Classification
+
+
+# Fatalities and Catastrophies Investigation Summaries
+class FCIS(Results, Accident, Inspection, Sic):
+    def __init__(self, keywords=[], *args):
+        Results.__init__(self, keywords=keywords, *args)
+        Accident.__init__(self, keywords=keywords, *args)
+        Inspection.__init__(self, *args)
+        Sic.__init__(self, *args)
+
+
+__author__ = ["Jae W. Choi"]
+__source__ = "https://github.com/jwc20/fcis-api"
+__license__ = "MIT"
+__all__ = ["FCIS"]
