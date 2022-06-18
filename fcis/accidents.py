70c7ee1b107e77a0ee901de7d12e8232685e2295
jwc20 <choij2015@gmail.com>
2022-06-17 17:49:53 -0700

    accidents outline

--- fcis/accidents.py
+++ fcis/accidents.py
@@ -3,5 +3,25 @@ from .core import *
 class Accidents(objects):
     def __init__(self, keywords=[], *args):
         self.keywords = keywords
+    
+    def _make_accidents_search_url(self):
+        return
 
+    def _load_accidents_search_page(self):
+        return
+
+    def _extract_accidents_0(self):
+        return 
+
+    def _extract_accidents_1(self):
+        return 
+
+    def _extract_accidents_2(self):
+        return 
+
+    def _transform_accidents(self):
+        return
+
+    def get_accidents(self):
+        return
 
