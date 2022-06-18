32674a9e1639d997a6a8362acba76456fdb096c2
jwc20 <choij2015@gmail.com>
2022-06-18 11:35:18 -0700

    added method to get list of keywords by first letter

--- fcis/accidents.py
+++ fcis/accidents.py
@@ -5,6 +5,18 @@ class Accidents(object):
     def __init__(self, keywords=[], *args):
         self.keywords = keywords
 
+    def is_a_keyword(keywords):
+        """
+        Validates a list of words to see if they are in the keyword list
+        """
+        for keyword in keywords:
+            if (keyword in self.get_keywords(keyword[0])):
+                return True
+            else:
+                return False
+
+        return
+
     def _make_accidents_search_url(self):
         return
 
@@ -14,6 +26,9 @@ class Accidents(object):
         return BeautifulSoup(html, "lxml")
 
     def _load_accidents_keywords_page(self, first_letter):
+        """
+        For loading the list of keywords page by letters.
+        """
         r = requests.get(BASE_URL + ACCIDENT_KEYWORD_LETTER_URL + first_letter)
         html = r.text
         return BeautifulSoup(html, "lxml")
@@ -39,9 +54,14 @@ class Accidents(object):
         if len(first_letter) != 1:
             print("Must input a letter.")
         else:
+            keyword_list = []
             page = self._load_accidents_keywords_page(first_letter)
             # TODO: scrape the keywords
-            return page
+            # must return a list
+            a_hrefs = page.find("div", {"class": "well"}).find_all("a")
+            for a_href in a_hrefs:
+                keyword_list.append(a_href.text)
+            return keyword_list
 
     def get_accidents(self):
         return
