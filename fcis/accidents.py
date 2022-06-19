08160c331e0f530d4b39ecf325c171c2a7c2e132
jwc20 <choij2015@gmail.com>
2022-06-19 14:36:00 -0700

    deleted the method for loading page by keyword, added conditional to keyword url with double quotes

--- fcis/accidents.py
+++ fcis/accidents.py
@@ -1,7 +1,11 @@
+
 from .core import *
 
 import urllib.parse
 
+def Merge(dict1, dict2):
+    res = {**dict1, **dict2}
+    return res
 
 class Accidents(object):
     def __init__(self, descriptions=[], abstracts=[], keywords=[], *args):
@@ -24,8 +28,8 @@ class Accidents(object):
         # print(not_keyword_list)
         return search_keyword_list
 
-    def _make_accidents_search_url(self, starting_at, _range):
-
+    def _make_accidents_search_url(self, p_start, p_finish, p_sort, p_desc, p_direction,  p_show):
+        
         payload = {}
 
         if self.descriptions:
@@ -43,17 +47,28 @@ class Accidents(object):
             payload[ACCIDENT_ABSTRACT_URL] = payload[ACCIDENT_ABSTRACT_URL].strip()
 
         if self.keywords:
-            existing_keywords = self._get_validated_keywords(self.keywords)
+            # If there are double quotes surrounding the word
+            if len(self.keywords) == 1 and self.keywords[0].find("\"") != -1:
+                existing_keywords = self.keywords
+                payload[ACCIDENT_KEYWORD_LIST_URL] = "on"
+            else:
+                existing_keywords = self._get_validated_keywords(self.keywords)
+            
             payload[ACCIDENT_KEYWORD_URL] = ""
             for item in existing_keywords:
                 payload[ACCIDENT_KEYWORD_URL] += item + " "
             payload[ACCIDENT_KEYWORD_URL] = payload[ACCIDENT_KEYWORD_URL].strip()
 
-        if starting_at:
-            payload[PAGE_FINISH_URL] = starting_at
+        if p_finish:
+            payload[PAGE_START_URL] = ""
+            payload[PAGE_FINISH_URL] = p_finish
+            payload[PAGE_SORT_URL] = p_sort if p_sort else ""
+            payload[PAGE_DESC_URL] = p_desc if p_desc else ""
+            payload[PAGE_DIRECTION_URL] = p_direction if p_direction else ""
+            payload[PAGE_SHOW_URL] = p_show if p_show else "20"
 
-        if _range:
-            payload[PAGE_SHOW_URL] = _range
+        if p_show and not p_finish:
+            payload[PAGE_SHOW_URL] = p_show
 
         return payload
 
@@ -68,12 +83,66 @@ class Accidents(object):
         html = r.text
         return BeautifulSoup(html, "lxml")
 
-    def _load_accidents_search_page(self, starting_at=None, _range=None):
+    # def _load_accidents_search_page_by_keyword(self, p_start=None, p_finish=None, p_sort=None, p_desc=None, p_direction=None, p_show=None):
+    #     # print(self._get_validated_keywords(self.keywords))
+    #     # print(is_accident_search(r.url))
+    #     # if len(
+    #     #     self._get_validated_keywords(self.keywords)
+    #     # ) == 1 and not is_accident_search(r.url):
+        
+    #     payload_keyword = {} 
 
+    #     keyword_url = self._get_validated_keywords(self.keywords)[0]
+    #     # print(url)
+        
+        
+    #     # # TODO: use urlencode or urllib.parse
+    #     # search_url = (
+    #     #     BASE_URL
+    #     #     + ACCIDENT_SEARCH_URL
+    #     #     + "?"
+    #     #     + ACCIDENT_KEYWORD_URL
+    #     #     + "="
+    #     #     + '"'
+    #     #     + keyword_url
+    #     #     + '"'
+    #     #     + ACCIDENT_KEYWORD_LIST_URL
+    #     # )
+    #     # print(search_url)
+    #     # payload_str = urlencode(
+    #     #     self._make_accidents_search_url(p_start, p_finish, p_sort, p_desc, p_direction, p_show), safe=""
+    #     # )
+        
+    #     search_url = BASE_URL + ACCIDENT_SEARCH_URL
+        
+    #     payload_keyword[ACCIDENT_KEYWORD_URL] = keyword_url
+    #     payload_keyword[ACCIDENT_KEYWORD_LIST_URL] = "on"
+        
+    #     # payload_str = urlencode(
+    #     #     self._make_accidents_search_url(p_start, p_finish, p_sort, p_desc, p_direction, p_show), safe=""
+    #     # )
+        
+    #     payload_str = self._make_accidents_search_url(p_start, p_finish, p_sort, p_desc, p_direction, p_show)
+        
+        
+        
+    #     payload = Merge(payload_str, payload_keyword)
+        
+    #     print(payload)
+        
+    #     r = requests.get(search_url, params=payload, headers=HEADERS)
+    #     print(r.url)
+    #     html = r.text
+    #     # print(html)
+    #     return BeautifulSoup(html, "lxml")
+    
+
+
+
+    def _load_accidents_search_page(self, p_start=None, p_finish=None, p_sort=None, p_desc=None, p_direction=None, p_show=None):
         search_url = BASE_URL + ACCIDENT_SEARCH_URL
-
         payload_str = urlencode(
-            self._make_accidents_search_url(starting_at, _range), safe="+"
+            self._make_accidents_search_url(p_start, p_finish, p_sort, p_desc, p_direction, p_show), safe="+"
         )
         # print(payload_str)
 
@@ -86,33 +155,9 @@ class Accidents(object):
 
         # If there is only one keyword and the site form can not fetch, go to the list of keywords and fetch from there.
         if is_accident_search(r.url):
+            print(r.url)
             html = r.text
             return BeautifulSoup(html, "lxml")
-        # print(self._get_validated_keywords(self.keywords))
-        # print(is_accident_search(r.url))
-        if len(
-            self._get_validated_keywords(self.keywords)
-        ) == 1 and not is_accident_search(r.url):
-            keyword_search_url = BASE_URL + ACCIDENT_SEARCH_URL
-            keyword_url = self._get_validated_keywords(self.keywords)[0].replace(
-                " ", "%20"
-            )
-            # print(keyword_url)
-            keyword_payload_str = (
-                keyword_search_url
-                + "?"
-                + ACCIDENT_KEYWORD_URL
-                + "="
-                + '"'
-                + keyword_url
-                + '"'
-                + ACCIDENT_KEYWORD_LIST_URL
-            )
-            print(keyword_payload_str)
-            keyword_r = requests.get(keyword_payload_str, headers=HEADERS)
-            keyword_html = keyword_r.text
-            # print(keyword_html)
-            return BeautifulSoup(keyword_html, "lxml")
 
         else:
             print("Your search did not return any result.")
@@ -155,14 +200,18 @@ class Accidents(object):
 
             for tr in table_rows[1:]:
                 data["accident_id"] = tr.find_all("td")[0].input.get("value")
-                data["summary_url"] = BASE_URL + "/pls/imis/" + tr.find_all("td")[2].a.get("href")
+                data["summary_url"] = (
+                    BASE_URL + "/pls/imis/" + tr.find_all("td")[2].a.get("href")
+                )
                 data["summary_nr"] = tr.find_all("td")[2].a.text
                 data["event_date"] = tr.find_all("td")[3].text
                 data["report_id"] = tr.find_all("td")[4].text
                 data["fatility"] = tr.find_all("td")[5].text
                 # data["sic_url"] = tr.find_all("td")[6].a.get("href")
                 data["sic_num"] = tr.find_all("td")[6].text
-                data["sic_url"] = BASE_URL + SIC_DETAILS_URL + "/" + tr.find_all("td")[6].text
+                data["sic_url"] = (
+                    BASE_URL + SIC_DETAILS_URL + "/" + tr.find_all("td")[6].text
+                )
                 data["event_description"] = tr.find_all("td")[7].text
                 results.append(data)
 
@@ -180,9 +229,12 @@ class Accidents(object):
         # 'fatility': 'X'
         new_results = []
 
-        # TODO: maybe use regular expressions here 
+        # TODO: maybe use regular expressions here
         for data in results:
-            if data["sic_url"] == "sic_manual.display?id=&tab=description" or data["sic_url"] == "https://www.osha.gov/sic-manual/":
+            if (
+                data["sic_url"] == "sic_manual.display?id=&tab=description"
+                or data["sic_url"] == "https://www.osha.gov/sic-manual/"
+            ):
                 data["sic_url"] = None
             if data["sic_num"] == "":
                 data["sic_num"] = None
@@ -201,6 +253,12 @@ class Accidents(object):
             for a_href in a_hrefs:
                 keyword_list.append(a_href.text.lower())
             return keyword_list
+
+    def DictListUpdate(lis1, lis2):
+        for aLis1 in lis1:
+            if aLis1 not in lis2:
+                lis2.append(aLis1)
+        return lis2
 
     def get_accidents(self):
         return
